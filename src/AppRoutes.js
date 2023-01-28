7affaa210fe465335a340c422c6c6c07fa925f66
PanosDeppy <panagiotiswb@gmail.com>
2023-01-28 14:36:34 +0000

    add page not found

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -3,14 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { MyPokeTeam } from "./pages/MyPokeTeam";
 import { AboutMe } from "./pages/AboutMe";
+import { PageNotFound } from "./pages/PageNotFound";
 
-// Our AppRoutes returns the 3 routes/pages that we have "Home", "MyPokeTeam and "AboutMe".
+// Our AppRoutes returns the 4 routes/pages that we have "Home", "MyPokeTeam, "AboutMe" and "PageNotFound".
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/my-poke-team" element={<MyPokeTeam />} />
       <Route path="/about-me" element={<AboutMe />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
