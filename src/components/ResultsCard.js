7f305a8e4957806f1722ca4a4ccd8502eacce00c
PanosDeppy <panagiotiswb@gmail.com>
2023-01-31 10:18:02 +0000

    created basic card layout

--- src/components/ResultsCard.js
+++ src/components/ResultsCard.js
@@ -5,26 +5,57 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { Box } from "@mui/system";
 
 export const ResultsCard = () => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      sx={{
+        maxWidth: 500,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          alignItems: "center",
+        }}
+      >
+        <img src="images/sean-thomas-F6920BvzrZE-unsplash.jpg"></img>
+      </Box>
       <CardMedia
-        sx={{ height: 140 }}
-        image="/static/images/cards/contemplative-reptile.jpg"
+        sx={{
+          height: 250,
+          display: "flex",
+        }}
         title="green iguana"
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+      <CardContent
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          sx={{ display: "flex", alignItems: "center" }}
+        >
           Pokemon Name
         </Typography>
       </CardContent>
-      <CardActions>
+      <CardActions
+        sx={{ display: "flex", justifyContent: "space-evenly", my: 3 }}
+      >
         <Button size="small">Type</Button>
         <Button size="small">Abilities</Button>
         <Button size="small">Stats</Button>
-        <Button size="small">Add to your team</Button>
       </CardActions>
+      <Button sx={{ my: 4 }} size="small">
+        Add to your team
+      </Button>
     </Card>
   );
 };
