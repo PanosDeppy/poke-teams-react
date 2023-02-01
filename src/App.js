078d844c7ddfac47ae0cc6e47e710446438c82d4
PanosDeppy <panagiotiswb@gmail.com>
2023-02-01 13:35:57 +0000

    add footer

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./AppRoutes";
 import { Navbar } from "./components/Navbar";
 import { Banner } from "./components/Banner";
+import { Footer } from "./components/Footer";
 
 // Our App returns the BrowserRouter that contains:
 // 1 - the AppRoutes which contains our 3 pages
-// 2 - and our Navbar, Banner as a constant in all of our pages
+// 2 - and our Navbar, Banner and Footer as a constant in all of our pages
 // (Navbar with navigate and Banner with props title).
 export const App = () => {
   return (
@@ -14,6 +15,7 @@ export const App = () => {
       <Navbar />
       <Banner />
       <AppRoutes />
+      <Footer />
     </BrowserRouter>
   );
 };
