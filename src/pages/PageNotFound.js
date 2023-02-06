c9d330d28b516efa85c5aa2481d6ea001576ddc5
PanosDeppy <panagiotiswb@gmail.com>
2023-02-06 11:19:38 +0000

    style my poke teams page and page not found

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,3 +1,24 @@
+import notfound from "../images/not-found.gif";
+
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+
 export const PageNotFound = () => {
-  return <div>Page Not Found</div>;
+  return (
+    <Stack spacing={0.5}>
+      <Box
+        sx={{
+          maxWidth: "80%",
+          display: "flex",
+          justifyContent: "center",
+          mt: 20,
+          mx: "auto",
+          border: "6px solid #9e2a2b",
+          borderRadius: 20,
+        }}
+        component="img"
+        src={notfound}
+      />
+    </Stack>
+  );
 };
